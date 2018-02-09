d56a7a889c472596d02aa7607ff9ac550df323cf
RijulM <rijulmandlekar@gmail.com>
2018-02-09 16:51:50 -0500

    up

--- Set1/one.py
+++ Set1/one.py
@@ -8,6 +8,6 @@ import pprint
 #hex = input("Enter a hex string:\n")
 hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 binary = binascii.unhexlify(hex)
-print("Your Hex String:\n " + str(binary))
+print("Your Hex String in Binary:\n " + str(binary))
 output = base64.b64encode(binascii.unhexlify(hex))
 print("Your Base64 String:\n " + str(output))
